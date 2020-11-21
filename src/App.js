2e2ddef03772337ce70d07c445d61c68f6cf5e5d
pelumi454 <amoochristianah454@gmail.com>
2020-11-21 16:25:23 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Route} from 'react-router-dom'
+import './App.css';
+import HomePage from './Components/HomePage'
+
+const HatsPage = () => {
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HomePage.scss'
+import Directory from './Directory'
+
+const HomePage = () => {
+    return (
+<div>
+    <div className="homepage">
+       <Directory></Directory>
+    </div>
+</div>
+    )
+}
+
+export default HomePage
